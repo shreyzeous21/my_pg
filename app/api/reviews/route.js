8f1123c5a99fc477b693b16c7ad997f4146861b2
Shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-11-10 21:44:06 +0530

    done

--- app/api/reviews/route.js
+++ app/api/reviews/route.js
@@ -4,14 +4,16 @@ import { NextResponse } from "next/server";
 // GET ALL REVIEWS
 export async function GET(req) {
   try {
-    // Fetch all reviews from the database, ordered by creation date if necessary
     const reviews = await prisma.review.findMany({
-      orderBy: { createdAt: "desc" }, // If you want to order by creation date
+      orderBy: { createdAt: "desc" },
     });
     return NextResponse.json(reviews, { status: 200 });
   } catch (error) {
     console.error("Error fetching reviews:", error);
-    return NextResponse.json({ message: "Error fetching reviews" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Error fetching reviews" },
+      { status: 500 }
+    );
   }
 }
 
@@ -40,6 +42,9 @@ export async function POST(req) {
     return NextResponse.json(review, { status: 201 });
   } catch (error) {
     console.error("Error creating review:", error);
-    return NextResponse.json({ message: "Error creating review" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Error creating review" },
+      { status: 500 }
+    );
   }
 }
