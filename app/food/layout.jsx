2d5bcb3a0f0524ee5ee55236002efe5e3f6b9465
Shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-11-13 20:31:46 +0530

    done

--- app/food/layout.jsx
+++ app/food/layout.jsx
@@ -17,7 +17,7 @@ export const metadata = {
 export default function FoodLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="shortcut icon" href="/logoFood.png" type="image/x-icon" />
+      <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
       <body className={` ${font.variable} antialiased bg-gray-400`}>
         <Header />
         {children}
